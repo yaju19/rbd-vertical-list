fbe160688fd5017b4200aaa4a7ac940ae7afe11c
yaju19 <yajuvendra.rathor@teachedison.com>
2022-06-21 15:09:00 +0530

    first commit

---
+++ src/List.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+export default function List() {
+  const [peopleList, setPeopleList] = useState(peopleArr);
+
+  const onDragEnd = (result) => {
+    console.log(result, "aaa");
+    const { destination } = result;
+
+    if (!destination) return;
+    const items = Array.from(peopleList);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    console.log(reorderedItem);
+    items.splice(result.destination.index, 0, reorderedItem);
+    console.log(items);
+
+    setPeopleList(items);
+  };
+  return (
+    <div>
+      <h2>People List</h2>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="peopleList">
+          {(provided) => (
+            <ul
+              className="people"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={{ listStyleType: "none" }}
+            >
+              {peopleList.map((el, index) => {
+                return (
+                  <Draggable key={el} draggableId={el} index={index}>
+                    {(provided) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <p>{el}</p>
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
+
+const peopleArr = ["Person A", "Person B", "Person C", "Person D", "Person E"];
